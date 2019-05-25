0fac9f5ebc98f293cbfb03ca5d272316af17fa37
EthanB <ebonsignori@gmail.com>
2019-05-25 13:10:14 -0400

    Pie.js model methods

--- models/Pie.js
+++ models/Pie.js
@@ -1,2 +1,23 @@
 const ORM = require('../config/orm')
 
+const Pie = {
+
+  selectAll: (cb) => {
+    ORM.selectAll('pies', (res) => {
+      cb(res)
+    })
+  },
+  insertOne: (cols, vals, cb) => {
+    ORM.insertOne('pies', cols, vals, (res) => {
+      cb(res)
+    })
+  },
+
+  updateOne: (objColVals, condition, cb) => {
+    ORM.updateOne('pies', objColVals, condition, (res) => {
+      cb(res)
+    })
+  }
+}
+
+module.exports = Pie
