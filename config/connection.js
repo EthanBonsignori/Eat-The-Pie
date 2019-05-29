7dff62cfb88f1ac42446891b5009b366fb9d984c
Ethan <ebonsignori@gmail.com>
2019-05-29 18:27:53 -0400

    added connection option for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,21 @@ require('dotenv/config')
 
 const dbName = 'pies_db'
 
-// MySQL DB Connection settings
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: `${process.env.DB_PASS}`,
-  database: `${dbName}`
-})
+let connection
+
+if (process.env.JASWB_URL) {
+  // JawsDB connection settings
+  connection = mysql.createConnection(process.env.JASWB_URL)
+} else {
+  // MySQL DB connection settings
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: `${process.env.DB_PASS}`,
+    database: `${dbName}`
+  })
+}
 
 // Initiate MySQL Connection
 connection.connect(err => {
